f058fc4cd4b5c32ba5ab4706c5630192aa1bd41f
akapusto <artjomsk3@gmail.com>
2025-09-23 20:24:57 +0300

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -12,17 +12,36 @@ bot = telebot.TeleBot(token)
 encrypted_text=''   # изначально создаем глобальные переменные
 decrypted_text=''   # для текстов и ключа, чтобы потом можно было
 key=''              # внутри функций их вызывать и изменять так же глобально
-language = 0
+language = '/LV'
 
-console_texts = {
-     "Start": ['Sveiki! Šis robots var šifrēt vai atšifrēt jūsu ziņojumu. Izmēģiniet to, ievadot komandu /encrypt vai /decrypt!','Hi! This bot can encrypt or decrypt your message. Try it by entering the /encrypt or /decrypt command!','Привет! Этот бот может зашифровать или расшифровать твое сообщение. Попробуй, введя команду /encrypt или /decrypt !'],
-     "Encode": ['Ir izvēlēta šifrēšana. Tagad atsūtiet man tekstu, kas man jāšifrē.','Encryption selected. Now send me your text that I need to encrypt.','Выбрана зашифровка. Скинь теперь свой текст, который я должен зашифровать.'],
-     "Decode":['Ir izvēlēta atšifrēšana. Tagad atsūtiet man tekstu, kas man jāatšifrē.','Decryption selected. Now send me your text that I need to decrypt.','Выбрана расшифровка. Скинь теперь свой текст, который я должен расшифровать.'],
-     "Key_encrypt":['Es redzu jūsu tekstu! Tagad atsūtiet man atslēgu, ko izmantošu tā šifrēšanai.','I see your text! Now send me the key I will use to encrypt it.','Вижу твой текст! Скинь теперь ключ, по которому я буду его шифровать.'],
-     "Key_decrypt":['Es redzu jūsu tekstu! Tagad atsūtiet man atslēgu, ko izmantošu tā atšifrēšanai.','I see your text! Now send me the key I will use to decrypt it.','Вижу твой текст! Скинь теперь ключ, по которому я буду его расшифровывать.'],
-     "Encoded_text":['Lieliski! Lūk, jūsu šifrētais teksts:','Excellent! Here is s your encrypted text:','Отлично! Вот твой зашифрованный текст:'],
-     "Decoded_text":['Lieliski! Lūk, jūsu atfrētais teksts:','Excellent! Here is s your decrypted text:','Отлично! Вот твой расшифрованный текст:']
-
+console_texts={
+    '/LV': {
+        'Start':'Sveiki! Šis robots var šifrēt vai atšifrēt jūsu ziņojumu. Izmēģiniet to, ievadot komandu /encrypt vai /decrypt!',
+        'Encode':'Ir izvēlēta šifrēšana. Tagad atsūtiet man tekstu, kas man jāšifrē.',
+        'Decode':'Ir izvēlēta atšifrēšana. Tagad atsūtiet man tekstu, kas man jāatšifrē.',
+        'Key_encrypt':'Es redzu jūsu tekstu! Tagad atsūtiet man atslēgu, ko izmantošu tā šifrēšanai.',
+        'Key_decrypt':'Es redzu jūsu tekstu! Tagad atsūtiet man atslēgu, ko izmantošu tā atšifrēšanai.',
+        'Encoded_text':'Lieliski! Lūk, jūsu šifrētais teksts:',
+        'Decoded_text':'Lieliski! Lūk, jūsu atfrētais teksts:'
+    },
+    '/ENG': {
+        'Start':'Hi! This bot can encrypt or decrypt your message. Try it by entering the /encrypt or /decrypt command!',
+        'Encode':'Encryption selected. Now send me your text that I need to encrypt.',
+        'Decode':'Decryption selected. Now send me your text that I need to decrypt.',
+        'Key_encrypt':'I see your text! Now send me the key I will use to encrypt it.',
+        'Key_decrypt':'I see your text! Now send me the key I will use to decrypt it.',
+        'Encoded_text':'Excellent! Here is s your encrypted text:',
+        'Decoded_text':'Excellent! Here is s your decrypted text:'
+    },
+    '/RU': {
+        'Start':'Привет! Этот бот может зашифровать или расшифровать твое сообщение. Попробуй, введя команду /encrypt или /decrypt !',
+        'Encode':'Выбрана зашифровка. Скинь теперь свой текст, который я должен зашифровать.',
+        'Decode':'Выбрана расшифровка. Скинь теперь свой текст, который я должен расшифровать.',
+        'Key_encrypt':'Вижу твой текст! Скинь теперь ключ, по которому я буду его шифровать.',
+        'Key_decrypt':'Вижу твой текст! Скинь теперь ключ, по которому я буду его расшифровывать.',
+        'Encoded_text':'Отлично! Вот твой зашифрованный текст:',
+        'Decoded_text':'Отлично! Вот твой расшифрованный текст:'
+    }
 }
 
 
@@ -52,12 +71,18 @@ alphabet = ['(', '%', 'h', 'W', 'ч', "'", '}', ']', 'U',
 
 
 def encode(text,key, mode):
+    """
+    возвращает зашифрованный или расшифрованный текст
+    text - обрабатываемый текст
+    key - ключ шифрования
+    mode - режим работы, предполагается "encrypt" и "decrypt"
+    """
     if mode == "encrypt":
         a = 1
     elif mode == "decrypt":
         a = -1
     else:
-        return "Error 404"                                              # decode function
+        return "Error 404"                                         # decode function
     ready_text = ''                                                #  
     text_indexes = []                                              #
     key_indexes = []                                               #
@@ -66,41 +91,27 @@ def encode(text,key, mode):
     for letter in key:                                             #
         key_indexes.append(alphabet.index(letter))                 #
     for i in range(len(text_indexes)):                             #
-        text_indexes[i]+=key_indexes[i%len(key_indexes)]*a           #
+        text_indexes[i]+=key_indexes[i%len(key_indexes)]*a         #
         ready_text+=alphabet[text_indexes[i]%len(alphabet)]        #
-    return ready_text                                           #
+    return ready_text                                              #
 
-
-
-
-@bot.message_handler(commands=['start'])
-# def start(message):
-#     bot.send_message(message.chat.id,  # encrypt и decrypt это зашифровка и расшифровка соответственно
-#                      'Привет! Этот бот может зашифровать или расшифровать твое сообщение. Попробуй, введя команду /encrypt или /decrypt !'
-#
-#                       )
+def validate():
+    """
+    проверяет текст на наличие символов отсутствующих в словаре, возвращает True/False
+    """
+    ...
+@bot.message_handler(commands=['start','menu'])
 def language_choice(message):
      bot.send_message(message.chat.id,  # encrypt и decrypt это зашифровка и расшифровка соответственно
                       'Choose the language: /LV ; /ENG ; /RU '
                       )
-     
-@bot.message_handler(commands=['LV','ENG','RU']) 
+     bot.register_next_step_handler(message,start)
 def start(message):
     global language
-    if message.text == '/LV':
-        language = 0
-        bot.send_message(message.chat.id,  # encrypt и decrypt это зашифровка и расшифровка соответственно
-                  console_texts["Start"][language])
-    elif message.text == '/ENG':
-        language = 1
-        bot.send_message(message.chat.id,  # encrypt и decrypt это зашифровка и расшифровка соответственно
-                  console_texts["Start"][language])
-    elif message.text == '/RU':
-        language = 2
-        bot.send_message(message.chat.id,  # encrypt и decrypt это зашифровка и расшифровка соответственно
-                  console_texts["Start"][language])
+    language=message.text
+    bot.send_message(message.chat.id, console_texts[language]['Start'])
 
-    
+
 def esc(text): 
     return re.sub(r'([_*\[\]()~`>#+\-=|{}.!])', r'\\\1', text)
 #
@@ -108,31 +119,31 @@ def esc(text):
 # весь этот блок - только про зашифровку сообщения
 #
 #
-@bot.message_handler(commands=['encrypt', 'decrypt'])
+@bot.message_handler(commands=['encrypt', 'decrypt',])
 def mode_choice(message):
     if message.text == '/encrypt':
         bot.register_next_step_handler(message,ask_for_text)
-        bot.send_message(message.chat.id, console_texts["Encode"][language])
-        
+        bot.send_message(message.chat.id, console_texts[language]["Encode"])
+
     elif message.text == '/decrypt':
         bot.register_next_step_handler(message,ask_for_text2)
-        bot.send_message(message.chat.id, console_texts["Decode"][language])
-        
+        bot.send_message(message.chat.id, console_texts[language]["Decode"])
+
 
 def ask_for_text(message):
     global encrypted_text
     encrypted_text = message.text
     bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    bot.send_message(message.chat.id, console_texts["Key_encrypt"][language])
+    bot.send_message(message.chat.id, console_texts[language]["Key_encrypt"])
     bot.register_next_step_handler(message,ask_for_key)
 
-    
+
 
 def ask_for_key(message):
     global key
     key=message.text
     bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    bot.send_message(message.chat.id, console_texts["Encoded_text"][language])
+    bot.send_message(message.chat.id, console_texts[language]["Encoded_text"])
     text = encode(encrypted_text,key,'encrypt')
     bot.send_message(message.chat.id, f"||{ esc(text) }||", parse_mode = "MarkdownV2")
 #
@@ -151,7 +162,7 @@ def ask_for_text2(message):
     global decrypted_text
     decrypted_text = message.text
     bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    bot.send_message(message.chat.id, console_texts["Key_decrypt"][language])
+    bot.send_message(message.chat.id, console_texts[language]["Key_decrypt"])
     bot.register_next_step_handler(message,ask_for_key2)
 
 
@@ -159,7 +170,7 @@ def ask_for_key2(message):
     global key
     key=message.text
     bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    bot.send_message(message.chat.id, console_texts["Decoded_text"][language])
+    bot.send_message(message.chat.id, console_texts[language]["Decoded_text"])
     text = encode(decrypted_text,key,'decrypt')
     bot.send_message(message.chat.id, f"||{ esc(text) }||", parse_mode = "MarkdownV2")
 #
